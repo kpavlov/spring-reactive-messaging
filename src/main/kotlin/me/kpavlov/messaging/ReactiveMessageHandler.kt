4a1d6d2c2f9313084b5b42f89ac9db48f5ff4f40
Konstantin Pavlov <{ID}+{username}@users.noreply.github.com>
2022-10-10 11:04:55 +0300

    Add `@FunctionalInterface` annotation to ReactiveMessageHandler

--- src/main/kotlin/me/kpavlov/messaging/ReactiveMessageHandler.kt
+++ src/main/kotlin/me/kpavlov/messaging/ReactiveMessageHandler.kt
@@ -4,6 +4,7 @@ import org.springframework.integration.acks.AcknowledgmentCallback
 import org.springframework.messaging.Message
 import reactor.core.publisher.Mono
 
+@FunctionalInterface
 public fun interface ReactiveMessageHandler<T> {
     public fun process(cmd: Message<T>): Mono<AcknowledgmentCallback.Status>
 }
