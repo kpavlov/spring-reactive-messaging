495d825a6644bfac39270e20e7a5bd267694644d
Konstantin Pavlov <kpavlov@@users.noreply.github.com>
2022-10-11 09:53:06 +0300

    Refactor SqsMessageHeaders
    add methods to retrieve tAcknowledgmentCallback and message receipt handle

--- src/main/kotlin/me/kpavlov/messaging/sqs/consumer/SqsMessageHeaders.kt
+++ src/main/kotlin/me/kpavlov/messaging/sqs/consumer/SqsMessageHeaders.kt
@@ -1,5 +1,8 @@
 package me.kpavlov.messaging.sqs.consumer
 
+import me.kpavlov.messaging.ReactiveAcknowledgmentCallback
+import org.springframework.integration.IntegrationMessageHeaderAccessor
+import org.springframework.messaging.Message
 import org.springframework.messaging.MessageHeaders
 import java.util.*
 
@@ -7,8 +10,32 @@ internal class SqsMessageHeaders(headers: Map<String, Any>, id: UUID, timestamp:
     MessageHeaders(headers, id, timestamp) {
     companion object {
         /**
-         * The key for the message ReceiptHandle.
+         * The key for the message _Receipt Handle_ header.
          */
         const val RECEIPT_HANDLE = "receiptHandle"
+
+        fun getAcknowledgmentCallback(message: Message<*>): ReactiveAcknowledgmentCallback? {
+            return message.headers.get(
+                IntegrationMessageHeaderAccessor.ACKNOWLEDGMENT_CALLBACK,
+                ReactiveAcknowledgmentCallback::class.java
+            )
+        }
+
+        /**
+         * Returns SQS Message _Receipt handle_
+         *
+         * > Every time you receive a message from a queue, you receive a receipt handle
+         * > for that message. This handle is associated with the action of receiving the message,
+         * > not with the message itself. To delete the message or to change the message visibility,
+         * > you must provide the receipt handle (not the message ID).
+         *
+         * @see <a href="https://docs.aws.amazon.com/AWSSimpleQueueService/latest/SQSDeveloperGuide/sqs-queue-message-identifiers.html#receipt-handle">Receipt handle</a>
+         */
+        fun getReceiptHandle(message: Message<*>): String? {
+            return message.headers.get(
+                RECEIPT_HANDLE,
+                String::class.java
+            )
+        }
     }
 }
